cff9274205891e88dc0016989cff1e4edb52f3f9
Abdisalaam Hassan <abdisalamxhassan@gmail.com>
2025-03-15 12:05:43 +0300

    Weird Algorithm

---
+++ Introductory Problems/Weird_Algorithm.py
@@ -0,0 +1,12 @@
+def number(n):
+    print(n)
+    while n != 1:
+        if n % 2 == 0:
+            n = n // 2
+        else:
+            n = n * 3 + 1
+        print(n)
+    return n
+
+n = int(input())
+print(number(n), sep=' ')
