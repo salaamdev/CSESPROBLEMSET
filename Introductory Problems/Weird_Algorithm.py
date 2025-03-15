ef5b18b8a8c8e9b43457fb4cc51f4adde425e824
Abdisalaam Hassan <abdisalamxhassan@gmail.com>
2025-03-15 12:08:10 +0300

    Weird Algorithm fix

--- Introductory Problems/Weird_Algorithm.py
+++ Introductory Problems/Weird_Algorithm.py
@@ -1,12 +1,12 @@
 def number(n):
-    print(n)
+    print(n, sep=' -> ', end=' ')
     while n != 1:
         if n % 2 == 0:
             n = n // 2
         else:
             n = n * 3 + 1
-        print(n)
-    return n
+        print(n, end=' ')
+    # return n
 
 n = int(input())
-print(number(n), sep=' ')
+number(n)
